c1a1c45f01989ad406d96d5a76965fc80500b393
Keerthan22-sys <keerthangowdas222@gmail.com>
2023-03-10 00:35:48 +0530

    Updates

---
+++ precache-manifest.d8c51798b7e6aa5adfa3918ef36de491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "231d9c6490281f4da3bd",
+    "url": "/textutil/static/css/main.9d63e4fc.chunk.css"
+  },
+  {
+    "revision": "231d9c6490281f4da3bd",
+    "url": "/textutil/static/js/main.45694007.chunk.js"
+  },
+  {
+    "revision": "fac04a71d3879917def2",
+    "url": "/textutil/static/js/runtime~main.8dea6a83.js"
+  },
+  {
+    "revision": "95d99c7d9d392ce9b7c2",
+    "url": "/textutil/static/js/2.1e29dc99.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/textutil/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "973c057a111af98d91e3aa7d00f02e20",
+    "url": "/textutil/index.html"
+  }
+];
